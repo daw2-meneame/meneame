dff01231dab7c34bf22b369d32e376f9a0ae6c0b
Álex Martín <alex@alexdw.com>
2020-05-29 13:57:15 +0200

    Add Category

---
+++ proyecto-meneame/components/Category.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class="nav  nav-pills mx-auto" >
+            <span>Categories:</span>
+                    <nuxt-link
+                      v-for="category in categories" :key="category.id"
+                      class="nav-link" :class="{active: category.category === current}"
+                      to="#" @click.prevent="changeCurrentCategory(category.category)"
+                      >{{category.title}}
+                      </nuxt-link>
+      </div>
+</template>
+<script>
+export default {
+  props:["categories", "current"],
+  methods:{
+    changeCurrentCategory(category){
+        this.$emit("change", category)
+      },
+  }
+}
+</script>
